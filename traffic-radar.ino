af221020e4dfe7ead7e4be69daa1a8e4f01952d8
syst3x <81773191+syst3x@users.noreply.github.com>
2024-11-05 20:34:12 -0500

    Add files via upload

--- traffic-radar.ino
+++ traffic-radar.ino
@@ -13,13 +13,18 @@ const boolean hibernate = true;
 const int lastDataTimeThreshold = 1500;
 // how long to sleep (keep in mind how long a vehicle will spend in view of
 // your radar and set this value comfortably lower than this)
-const int sleepTime = 750;
+const int sleepTime = 400;
+//const int sleepTime = 750;
 // at minimum, how long should the radar stay on after waking up from sleep
-const int minOnTime = 1000;
+const int minOnTime = 750;
 //longest time a car could be in view of sensor before we record a datapoint
-const int timeStartThreshold = 3500;
+const int timeStartThreshold = 15000;
 //log and reset if we haven't had a speed reading in this long
-const int timeLastThreshold = 650;
+const int timeLastThreshold = 600;
+// magnitude change threshold (e.g., trailing car will cause step drop in inbound magnitude when picked up)
+const int magThreshold = 200;
+// log ALL datapoints?
+const boolean logAll = true;
 
 /////////////////////////////////////////////////////////////////////////////
 // do not modify below this line
@@ -30,20 +35,27 @@ RTC_DS3231 rtc;
 const int chipSelect = SDCARD_SS_PIN;
 File dataFile;
 float currentSpeed;
+float currentMag;
 
 float inboundSpeedMax;
 char inboundSpeedMaxChar[8];
 int inboundTimeStart;
 int inboundTimeLast;
+float inboundMag;
+float inboundMagLast;
 
 float outboundSpeedMax;
 char outboundSpeedMaxChar[8];
 int outboundTimeStart;
 int outboundTimeLast;
+float outboundMag;
+float outboundMagLast;
 
 char radarData[256];
 char speed[8];
+char mag[8];
 char logMessage[128];
+char * pch;
 boolean usbSerial;
 int timeLastFlush;
 int lastDataTime;
@@ -97,7 +109,7 @@ void setup() {
 
   // setup all our radar options
   // radar sign mode
-  Serial1.println("ON");
+  Serial1.println("On");
   // mph units
   Serial1.println("US");
   // JSON mode
@@ -106,10 +118,11 @@ void setup() {
   Serial1.println("R>10");
   // sample rate to 20,000 for max speed 139.1mph
   Serial1.println("S2");
-  // turn off LEDs to save a little power
-  Serial1.println("Ol");
+  // turn on magnitude reports
+  Serial1.println("OM");
   // turn on hibernate mode
   //Serial1.println("Z-");
+  Serial1.println("PA");
 
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
@@ -143,13 +156,17 @@ void setup() {
     dataFile.print(",");
     dataFile.print(rtc.now().timestamp(DateTime::TIMESTAMP_TIME));
     dataFile.println("");
-    dataFile.println("timeSinceStart,timeSinceLast,date,time,speed");
+    dataFile.println("dateTime,timeSinceStart,timeSinceLast,magnitude,lastMagnitude,speed,type");
   } else {
     if(usbSerial) {
       Serial.print("Error opening log file ");
       Serial.println(logFilename);
     }
   }
+  // wait 10 seconds with radar LED ON to confirm operation, then turn off to save power and proceed to data collection
+  Serial1.println("OL");
+  delay(10000);
+  Serial1.println("Ol");
 
   timeLastFlush = 0;
   digitalWrite(LED_BUILTIN, LOW);   //turn off LED by default
@@ -163,38 +180,86 @@ void loop() {
   if (Serial1.available()) {       // If anything comes in Serial1 (pins 0 & 1)
     int bytes = Serial1.readBytesUntil('\n', radarData, 256);
     if (bytes > 0)  {
-      //printToSerial("recieved at least one byte");
       if(usbSerial) Serial.println(radarData);
-      //Serial.println(bytes);
-      char * pch;
-      pch = strstr(radarData, "DetectedObjectVelocity");
+      pch = strstr(radarData, "speed");
       if (pch != NULL) {
+        //if(usbSerial) Serial.println(pch);
         lastDataTime = now;
-        memcpy(speed, &radarData[27], bytes - 30);
-        //Serial.println(speed);
-        float currentSpeed = atof(speed);
-
+        memcpy(speed, &pch[10], strlen(pch) - 13);
+        memcpy(mag, &radarData[16], bytes - strlen(pch) - 20);
+        currentSpeed = atof(speed);
+        currentMag = atof(mag);
+        if(usbSerial) Serial.print(speed);
+        if(usbSerial) Serial.print("  ");
+        if(usbSerial) Serial.println(mag);
         if (currentSpeed < 0 ) {
-          outboundTimeLast = now;
           if (outboundSpeedMax == 0)  {
             outboundTimeStart = now;
           }
+         if (logAll) {
+            timestamp = rtc.now();
+            dataFile.print(timestamp.timestamp(DateTime::TIMESTAMP_DATE));
+            dataFile.print(" ");
+            dataFile.print(timestamp.timestamp(DateTime::TIMESTAMP_TIME));
+            dataFile.print(",");
+            dataFile.print((now - outboundTimeStart));
+            dataFile.print(",");
+            dataFile.print((now - outboundTimeLast));
+            dataFile.print(",");
+            dataFile.print(currentMag);
+            dataFile.print(",");
+            dataFile.print(outboundMag);
+            dataFile.print(",");
+            dataFile.print(speed);
+            dataFile.print(",");
+            dataFile.println("frame");
+          }
+
+          outboundTimeLast = now;
+
           if (currentSpeed < outboundSpeedMax) {
             outboundSpeedMax = currentSpeed;
             strcpy(outboundSpeedMaxChar, speed);
           }
+          outboundMagLast = outboundMag;
+          outboundMag = currentMag;
         }
         if (currentSpeed > 0 ) {
-          inboundTimeLast = now;
           if (inboundSpeedMax == 0)  {
             inboundTimeStart = now;
           }
+
+          if (logAll) {
+            timestamp = rtc.now();
+            dataFile.print(timestamp.timestamp(DateTime::TIMESTAMP_DATE));
+            dataFile.print(" ");
+            dataFile.print(timestamp.timestamp(DateTime::TIMESTAMP_TIME));
+            dataFile.print(",");
+            dataFile.print((now - inboundTimeStart));
+            dataFile.print(",");
+            dataFile.print((now - inboundTimeLast));
+            dataFile.print(",");
+            dataFile.print(currentMag);
+            dataFile.print(",");
+            dataFile.print(inboundMag);
+            dataFile.print(",");
+            dataFile.print(speed);
+            dataFile.print(",");
+            dataFile.println("frame");
+          }
+
+          inboundTimeLast = now;
+
           if (currentSpeed > inboundSpeedMax) {
             inboundSpeedMax = currentSpeed;
             strcpy(inboundSpeedMaxChar, speed);
           }
+          inboundMagLast = inboundMag;
+          inboundMag = currentMag;
         }
         memset(speed, 0, sizeof(speed));
+        memset(mag, 0, sizeof(mag));
+        memset(pch, 0, sizeof(pch));
       }
       memset(radarData, 0, sizeof(radarData));
     }
@@ -204,50 +269,74 @@ void loop() {
   // if they have, write them to log file and reset values
   if (((now - outboundTimeStart) > timeStartThreshold || (now - outboundTimeLast) > timeLastThreshold) && outboundSpeedMax < 0) {
     timestamp = rtc.now();
+    dataFile.print(timestamp.timestamp(DateTime::TIMESTAMP_DATE));
+    dataFile.print(" ");
+    dataFile.print(timestamp.timestamp(DateTime::TIMESTAMP_TIME));
+    dataFile.print(",");
     dataFile.print((now - outboundTimeStart));
     dataFile.print(",");
     dataFile.print((now - outboundTimeLast));
     dataFile.print(",");
-    dataFile.print(timestamp.timestamp(DateTime::TIMESTAMP_DATE));
+    dataFile.print(outboundMag);
     dataFile.print(",");
-    dataFile.print(timestamp.timestamp(DateTime::TIMESTAMP_TIME));
+    dataFile.print(outboundMagLast);
     dataFile.print(",");
-    dataFile.println(outboundSpeedMaxChar);
+    dataFile.print(outboundSpeedMaxChar);
+    dataFile.print(",");
+    dataFile.println("vehicle");
     if (usbSerial) {
-      Serial.print((now - outboundTimeStart));
-      Serial.print(" ");
-      Serial.print((now - outboundTimeLast));
-      Serial.print(" ");
       Serial.print(timestamp.timestamp(DateTime::TIMESTAMP_DATE));
       Serial.print(" ");
       Serial.print(timestamp.timestamp(DateTime::TIMESTAMP_TIME));
-      Serial.print(" ");
-      Serial.println(outboundSpeedMaxChar);
+      Serial.print(",");
+      Serial.print((now - outboundTimeStart));
+      Serial.print(",");
+      Serial.print((now - outboundTimeLast));
+      Serial.print(",");
+      Serial.print(outboundMag);
+      Serial.print(",");
+      Serial.print(outboundMagLast);
+      Serial.print(",");
+      Serial.print(outboundSpeedMaxChar);
+      Serial.print(",");
+      Serial.println("vehicle");
     }
     outboundSpeedMax = 0;
     memset(outboundSpeedMaxChar, 0, sizeof(outboundSpeedMaxChar));
   }
   if (((now - inboundTimeStart) > timeStartThreshold || (now - inboundTimeLast) > timeLastThreshold) && inboundSpeedMax > 0) {
     timestamp = rtc.now();
+    dataFile.print(timestamp.timestamp(DateTime::TIMESTAMP_DATE));
+    dataFile.print(" ");
+    dataFile.print(timestamp.timestamp(DateTime::TIMESTAMP_TIME));
+    dataFile.print(",");
     dataFile.print((now - inboundTimeStart));
     dataFile.print(",");
     dataFile.print((now - inboundTimeLast));
     dataFile.print(",");
-    dataFile.print(timestamp.timestamp(DateTime::TIMESTAMP_DATE));
+    dataFile.print(inboundMag);
     dataFile.print(",");
-    dataFile.print(timestamp.timestamp(DateTime::TIMESTAMP_TIME));
+    dataFile.print(inboundMagLast);
     dataFile.print(",");
-    dataFile.println(inboundSpeedMaxChar);
+    dataFile.print(inboundSpeedMaxChar);
+    dataFile.print(",");
+    dataFile.println("vehicle");
     if (usbSerial) {
-      Serial.print((now - inboundTimeStart));
-      Serial.print(" ");
-      Serial.print((now - inboundTimeLast));
-      Serial.print(" ");
       Serial.print(timestamp.timestamp(DateTime::TIMESTAMP_DATE));
       Serial.print(" ");
       Serial.print(timestamp.timestamp(DateTime::TIMESTAMP_TIME));
-      Serial.print(" ");
-      Serial.println(inboundSpeedMaxChar);
+      Serial.print(",");
+      Serial.print((now - inboundTimeStart));
+      Serial.print(",");
+      Serial.print((now - inboundTimeLast));
+      Serial.print(",");
+      Serial.print(inboundMag);
+      Serial.print(",");
+      Serial.print(inboundMagLast);
+      Serial.print(",");
+      Serial.print(inboundSpeedMaxChar);
+      Serial.print(",");
+      Serial.println("vehicle");
     }
     inboundSpeedMax = 0;
     memset(inboundSpeedMaxChar, 0, sizeof(inboundSpeedMaxChar));
@@ -276,5 +365,6 @@ void loop() {
     Serial1.println("PA");
     asleep = false;
   }
+  
 }
 
